4259509057d6941f432052412d3616e308dae8bd
karti <kartikeya968@gmail.com>
2025-06-20 16:35:05 +0530

    by kk

--- Calculator/src/com/Calculator.java
+++ Calculator/src/com/Calculator.java
@@ -2,7 +2,21 @@ package com;
 
 public class Calculator
 {
-public static void main(String[] args) {
 	
-}
-}
+	public int division()
+	{
+		System.out.println("division method");
+		int a=10;
+		int b=2;
+		int res=a/b;
+		return res;
+	}
+	public int multiplication()
+	{
+		System.out.println("division method");
+		int a=10;
+		int b=2;
+		int res1=a*b;
+		return res1;
+	}
+  }
